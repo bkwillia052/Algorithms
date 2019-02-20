8f0c9d6bd9abb8e87ff62f3d9f26f4291a2e700a
bkwillia052 <bkwillia052@gmail.com>
2019-02-20 17:51:38 -0600

    Making_change in progress. Researching permutations T_T

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,33 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+
+    ways = 0
+    way_pairs = {}
+    if amount == 0:
+      return 0
+    for value in denominations:
+        way_pairs[value] = []
+        if amount % value == 0:
+          way_pairs[value] = [value]
+          """ print("Single denom:",value) """
+          ways += 1
+
+    for i in range(len(denominations)-1, -1, -1):
+         
+         """ print(denominations[i]) """
+         for value in denominations:
+             """ print("Values:", denominations[i], value) """
+             if denominations[i] == amount:
+                pass 
+             elif amount - denominations[i] < 0:
+                pass
+             elif (amount - denominations[i]) % value == 0:
+                if not value in way_pairs[denominations[i]]:
+                    way_pairs[denominations[i]].append(value)
+                    ways += 1
+    print(747 % 38)          
+    return ways
 
 
 if __name__ == "__main__":
@@ -15,3 +41,15 @@ if __name__ == "__main__":
     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
   else:
     print("Usage: making_change.py [amount]")
+
+
+    """ 
+    NOTES:
+    -Set a var for num of ways
+    -You'll have to see how many times each denomination can go into the
+    amount with % 0. 
+    ^^these will each increment the 'ways' var by 1
+    -Next you'll need to find the different combinations of coins that count as a way
+    --For this, you will need to find ways to track how many pairs of matches there are.
+    --You'll also need to find a ways to break down remainders into change pairs too. 
+     """
