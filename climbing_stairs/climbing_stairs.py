77c33b19f033f30f2e9437cc33fc767e7853592d
bkwillia052 <bkwillia052@gmail.com>
2019-02-21 16:12:10 -0600

    climbing stairs complete

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,25 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+
+    cache = {i: 0 for i in range(n+1)}
+    cache[0] = 1
+    jumps = [1,2,3]
+
+    for jump in jumps:
+
+        for step in range(jump, len(cache)):
+            diff = step - jump
+            if step >= 3: 
+                cache[step] = cache[step-1] + cache[step-2] + cache[step-3]
+            else:
+                cache[step] += cache[diff]
+    print(cache)
+    return cache[n]
+        
+
+
+   
 
 
 if __name__ == "__main__":
